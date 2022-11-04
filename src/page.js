0e674434f6e3866e1d5f2ba8d7609cec30c3a655
Trevor <trevorgrilley@yahoo.com>
2022-11-03 22:47:03 -0400

    add team questions

--- src/page.js
+++ src/page.js
@@ -6,3 +6,35 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 const generatePage = require('./src/generatePage');
+
+const questions = [
+    {
+        type: 'input',
+        message: "Please enter your team name",
+        name: 'teamName',
+    },
+    {
+        type: 'input',
+        message: "Please enter your teams ID",
+        name: 'teamID',
+    },
+    {
+        type: 'input',
+        message: "Please enter your teams email",
+        name: 'teamEmail',
+    },
+    {
+        type: 'input',
+        message: "Please enter your teams office number",
+        name: 'teamOfficeNumber',
+    },
+    {
+        type: 'list',
+        message: 'Please enter what team member you would like to add',
+        name: 'teamMember',
+        choices: [
+            'Engineer', 'Intern', 'None',
+        ],
+    },
+
+];
